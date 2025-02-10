f407ff1a06b7431c2269a5f1a46dfffcebba0f75
Артем <van@vrn.str>
2025-02-10 17:15:11 +0300

    Revert "add device id and device name to yandex registration"
    This reverts commit 78925d43b1932eb3df035f8c777fee8fbaa1f01f.

--- sandbox/OpenIddict.Sandbox.Console.Client/Program.cs
+++ sandbox/OpenIddict.Sandbox.Console.Client/Program.cs
@@ -105,8 +105,6 @@ var host = new HostBuilder()
                                "login:email",
                                "login:info"
                            );
-                           options.SetDeviceId("8447D3C9-C858-41B4-8DA9-80A7B9131C96");
-                           options.SetDeviceName("test device");
                        })
                        .AddVkId(options =>
                        {
